3e860a52e2cf79b2706593130e46f247afe3bf70
ChaeDoll <dlacodbs0627@gmail.com>
2023-03-13 10:55:23 +0900

    Upload C sharp Folder

---
+++ C Sharp/001.cs
@@ -0,0 +1,9 @@
+//두 수의 차를 구하는 함수 만들기 (230313)
+using System;
+
+public class Solution{
+    public int solution(int num1, int num2) {
+        int answer = num1 - num2;
+        return answer;
+    }
+}
