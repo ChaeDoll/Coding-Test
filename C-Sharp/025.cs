801e313a869e8a1d5f50542c9f365370905368f5
ChaeDoll <dlacodbs0627@gmail.com>
2023-03-18 22:55:55 +0900

    One day One git

---
+++ C-Sharp/025.cs
@@ -0,0 +1,12 @@
+//암호 해독 (230318)
+using System;
+
+public class Solution {
+    public string solution(string cipher, int code) {
+        string answer = "";
+        for (int i=code; i<=cipher.Length; i+=code) {
+            answer+=cipher[i-1];
+        }
+        return answer;
+    }
+}
