74c793765eacafe413a9c773edca79b81a911686
ChaeDoll <dlacodbs0627@gmail.com>
2023-03-23 02:26:41 +0900

    Study

--- C-Sharp/029.cs
+++ C-Sharp/029.cs
@@ -1,1 +1,16 @@
-//(230322)
+//가변배열 사용한 n의 배수 고르기(230322)
+using System;
+
+public class Solution {
+    public int[] solution(int n, int[] numlist) {
+        int[] answer = new int[0];
+        int count=0;
+        for (int i=0; i<numlist.Length; i++) {
+            if (numlist[i]%n==0) {
+                Array.Resize(ref answer, answer.Length+1);
+                answer[answer.Length-1]=numlist[i];
+            }
+        }
+        return answer;
+    }
+}
